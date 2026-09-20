9fc466673f8f03ceeeb35226d6e1d4305cac928c
Maulana Zain <maulanadezain@gmail.com>
2026-09-20 17:00:59 +0700

    feat: simplify expected result (dry)

--- dayone/hello_test.go
+++ dayone/hello_test.go
@@ -7,17 +7,34 @@ func TestHello(t *testing.T) {
 		got := Hello("Fulan", "")
 		want := "Hello, Fulan"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("saying hello world when `who` is empty", func(t *testing.T) {
 		got := Hello("", "")
 		want := "Hello, World"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+
+	t.Run("saying hello to Budi in Indonesia", func(t *testing.T) {
+		got := Hello("Budi", "id")
+		want := "Halo, Budi"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("saying hello to Budi in Arabic", func(t *testing.T) {
+		got := Hello("Budi", "ar")
+		want := "Ahlan, Budi"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
